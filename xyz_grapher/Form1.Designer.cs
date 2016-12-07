844afaa6f76730d8c6dba31d9db8d5b4f6e48bbd
keithpierno <keithpierno@gmail.com>
2016-12-06 19:22:51 -0500

    Initial checkin of full working application
    Everything needed to build and use the application.

---
+++ xyz_grapher/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace xyz_grapher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Comm Port:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(81, 10);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.DropDown += new System.EventHandler(this.comboBox1_DropDown);
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(208, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Baud Rate";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(272, 10);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 3;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(16, 37);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series1.Legend = "Legend1";
+            series1.Name = "X Axis";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(700, 150);
+            this.chart1.TabIndex = 4;
+            this.chart1.Text = "X-axis";
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(16, 203);
+            this.chart2.Name = "chart2";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series2.Legend = "Legend1";
+            series2.Name = "Y Axis";
+            this.chart2.Series.Add(series2);
+            this.chart2.Size = new System.Drawing.Size(700, 150);
+            this.chart2.TabIndex = 5;
+            this.chart2.Text = "Y-axis";
+            // 
+            // chart3
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart3.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart3.Legends.Add(legend3);
+            this.chart3.Location = new System.Drawing.Point(16, 371);
+            this.chart3.Name = "chart3";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series3.Legend = "Legend1";
+            series3.Name = "Z Axis";
+            this.chart3.Series.Add(series3);
+            this.chart3.Size = new System.Drawing.Size(700, 150);
+            this.chart3.TabIndex = 6;
+            this.chart3.Text = "Z-axis";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(641, 527);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(400, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Data Type";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(464, 10);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 9;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 562);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart3);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "XYZ Graph Tool";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart3;
+        private System.Windows.Forms.Button button1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox3;
+    }
+}
+
---
+++ xyz_grapher/Form1.cs
@@ -0,0 +1,413 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Data;
+using System.Drawing;
+using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+//using System.Diagnostics;
+
+namespace xyz_grapher
+{
+    public partial class Form1 : Form
+    {
+        private string port = string.Empty;
+        private Int32 baud = 0;
+        private Thread readThread;
+        private string dataType = "Decimal";
+
+        private Int16[] x_array_int = new Int16[500];
+        private Int16[] y_array_int = new Int16[500];
+        private Int16[] z_array_int = new Int16[500];
+
+        private double[] x_array_double = new double[500];
+        private double[] y_array_double = new double[500];
+        private double[] z_array_double = new double[500];
+
+        private readonly object m_Locker = new object();
+        private bool signalThread;
+
+        public Form1()
+        {
+            InitializeComponent();
+            populateBaudRates();
+
+            // Populate Dropdown with valid datatypes and select Doubles as default type
+            comboBox3.Items.Add("Decimal");
+            comboBox3.Items.Add("Integer");
+            comboBox3.SelectedItem = dataType;
+
+            modifyCharts();
+        }
+
+        private void modifyCharts()
+        {
+            // To change the color for the line on the charts
+            chart1.Series[0].Color = Color.Crimson;
+            chart2.Series[0].Color = Color.DodgerBlue;
+            chart3.Series[0].Color = Color.Green;
+
+            // disable vertical grid lines and turn off labels
+            // There are all sorts of options for the charts below that are commented out
+
+            //chart1.ChartAreas[0].AxisY.IsMarginVisible = false;
+            //chart1.ChartAreas[0].AxisX.MinorGrid.Enabled = false;
+            //chart1.ChartAreas[0].AxisX.MajorTickMark.Enabled = false;
+            //chart1.ChartAreas[0].AxisX.MinorTickMark.Enabled = false;
+            //chart1.ChartAreas[0].AxisX.Interval = 0;
+            //chart1.ChartAreas[0].AxisX.LabelStyle.ForeColor = Color.White;
+            //chart1.ChartAreas[0].AxisX.LineWidth = 0;
+            chart1.ChartAreas[0].AxisX.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.False;
+            //chart1.ChartAreas[0].AxisY.LabelStyle.Enabled = false;
+            //chart1.ChartAreas[0].AxisY.MinorGrid.Enabled = false;
+            //chart1.ChartAreas[0].AxisY.MajorTickMark.Enabled = false;
+            //chart1.ChartAreas[0].AxisY.MinorTickMark.Enabled = false;
+            //chart1.ChartAreas[0].AxisY.LineWidth = 0;
+
+            //chart2.ChartAreas[0].AxisY.IsMarginVisible = false;
+            //chart2.ChartAreas[0].AxisX.MinorGrid.Enabled = false;
+            //chart2.ChartAreas[0].AxisX.MajorTickMark.Enabled = false;
+            //chart2.ChartAreas[0].AxisX.MinorTickMark.Enabled = false;
+            //chart2.ChartAreas[0].AxisX.Interval = 0;
+            //chart2.ChartAreas[0].AxisX.LabelStyle.ForeColor = Color.White;
+            //chart2.ChartAreas[0].AxisX.LineWidth = 0;
+            chart2.ChartAreas[0].AxisX.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.False;
+            //chart2.ChartAreas[0].AxisY.LabelStyle.Enabled = false;
+            //chart2.ChartAreas[0].AxisY.MinorGrid.Enabled = false;
+            //chart2.ChartAreas[0].AxisY.MajorTickMark.Enabled = false;
+            //chart2.ChartAreas[0].AxisY.MinorTickMark.Enabled = false;
+            //chart2.ChartAreas[0].AxisY.LineWidth = 0;
+
+            //chart3.ChartAreas[0].AxisY.IsMarginVisible = false;
+            //chart3.ChartAreas[0].AxisX.MinorGrid.Enabled = false;
+            //chart3.ChartAreas[0].AxisX.MajorTickMark.Enabled = false;
+            //chart3.ChartAreas[0].AxisX.MinorTickMark.Enabled = false;
+            //chart3.ChartAreas[0].AxisX.Interval = 0;
+            //chart3.ChartAreas[0].AxisX.LabelStyle.ForeColor = Color.White;
+            //chart3.ChartAreas[0].AxisX.LineWidth = 0;
+            chart3.ChartAreas[0].AxisX.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.False;
+            //chart3.ChartAreas[0].AxisY.LabelStyle.Enabled = false;
+            //chart3.ChartAreas[0].AxisY.MinorGrid.Enabled = false;
+            //chart3.ChartAreas[0].AxisY.MajorTickMark.Enabled = false;
+            //chart3.ChartAreas[0].AxisY.MinorTickMark.Enabled = false;
+            //chart3.ChartAreas[0].AxisY.LineWidth = 0;
+
+        }
+
+        private void populateBaudRates()
+        {
+            //Put a list of baud rates into the combo box
+            //comboBox2.Items.Add(110);
+            //comboBox2.Items.Add(300);
+            //comboBox2.Items.Add(600);
+            //comboBox2.Items.Add(1200);
+            //comboBox2.Items.Add(2400);
+            //comboBox2.Items.Add(4800);
+            comboBox2.Items.Add(9600);
+            comboBox2.Items.Add(14400);
+            comboBox2.Items.Add(19200);
+            comboBox2.Items.Add(28800);
+            comboBox2.Items.Add(38400);
+            comboBox2.Items.Add(56000);
+            comboBox2.Items.Add(57600);
+            comboBox2.Items.Add(115200);
+        }
+
+        private void scanForCommPorts()
+        {
+            //show list of valid com ports
+            // Get a list of serial port names.
+            List<string> ports = new List<string>();
+
+            ports = SerialPort.GetPortNames().ToList();
+
+            if (ports.Count() == 0)
+            {
+                //add an entry to make sure that we don't have an empty list for testing.
+                ports.Add("[No Comm Port]");
+            }
+
+            comboBox1.Items.Clear();
+
+            // Display each port name to the console.
+            foreach (string portname in ports)
+            {
+                comboBox1.Items.Add(portname);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "Start")
+            {
+                if (port.ToString() != string.Empty && baud != 0)
+                {
+                    // can't open dummy port
+                    if (port.ToString() != "[No Comm Port]")
+                    {
+                        open_serial();
+                        lock (m_Locker)
+                        {
+                            signalThread = true;
+                        }
+
+                        readThread = new Thread(new ThreadStart(readSerial));
+                        readThread.Start();
+
+                        comboBox1.Enabled = false;
+                        comboBox2.Enabled = false;
+                        comboBox3.Enabled = false;
+                        button1.Text = "Stop";
+                    }
+
+                    //for testing chart formatting
+                    //else
+                    //{
+                    //    UpdateCharts();
+                    //}
+                }
+            }
+            else
+            {
+                //end thread
+                lock (m_Locker)
+                {
+                    signalThread = false;
+                }
+
+                if (!readThread.Join(1000))
+                {
+                    try
+                    {
+                        readThread.Abort();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.ToString());
+                    }
+                }
+
+                //close serial port
+                close_serial();
+
+                comboBox1.Enabled = true;
+                comboBox2.Enabled = true;
+                comboBox3.Enabled = true;
+                button1.Text = "Start";
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            port = comboBox1.SelectedItem.ToString();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            baud = Convert.ToInt32(comboBox2.SelectedItem);
+        }
+
+        private void open_serial()
+        {
+            // opens serial port to starts data capture
+            serialPort1.PortName = port;
+            serialPort1.BaudRate = baud;
+            try
+            {
+                serialPort1.Open();
+            }
+            catch
+            {
+                serialPort1.Close();
+            }
+
+        }
+
+        private void close_serial()
+        {
+            // closes serial port and ends data capture
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+            }
+        }
+
+        private void readSerial()
+        {
+            string input;
+            bool run = true;
+
+            //throw away the first few readings.
+            //for (int i=0; i < 10; i++)
+            //{
+            //    input = serialPort1.ReadLine();
+            //}
+
+            while (run)
+            {
+                input = serialPort1.ReadLine();
+                input = input.Replace("\n", String.Empty);
+                input = input.Replace("\r", String.Empty);
+
+                //split string on commas
+                string[] input_array = input.Split(',');
+                int array_length = input_array.Length;
+
+                if (array_length > 0)
+                {
+                    if (dataType == "Decimal")
+                    {
+                        try
+                        {
+                            x_array_double[x_array_double.Length - 1] = Convert.ToDouble(input_array[0]);
+                            Array.Copy(x_array_double, 1, x_array_double, 0, x_array_double.Length - 1);
+                        }
+                        catch
+                        {
+                            //if the cast fails do nothing
+                            Array.Copy(x_array_double, 1, x_array_double, 0, x_array_double.Length - 1);
+                        }
+                    }
+                    else
+                    {
+                        try
+                        {
+                            x_array_int[x_array_int.Length - 1] = Convert.ToInt16(input_array[0]);
+                            Array.Copy(x_array_int, 1, x_array_int, 0, x_array_int.Length - 1);
+                        }
+                        catch
+                        {
+                            Array.Copy(x_array_int, 1, x_array_int, 0, x_array_int.Length - 1);
+                        }
+                    }
+                }
+
+                if (array_length > 1)
+                {
+                    if (dataType == "Decimal")
+                    {
+                        try
+                        {
+                            y_array_double[y_array_double.Length - 1] = Convert.ToDouble(input_array[1]);
+                            Array.Copy(y_array_double, 1, y_array_double, 0, y_array_double.Length - 1);
+                        }
+                        catch
+                        {
+                            Array.Copy(y_array_double, 1, y_array_double, 0, y_array_double.Length - 1);
+                        }
+                    }
+                    else
+                    {
+                        try
+                        {
+                            y_array_int[y_array_int.Length - 1] = Convert.ToInt16(input_array[1]);
+                            Array.Copy(y_array_int, 1, y_array_int, 0, y_array_int.Length - 1);
+                        }
+                        catch
+                        {
+                            Array.Copy(y_array_int, 1, y_array_int, 0, y_array_int.Length - 1);
+                        }
+                    }
+                }
+
+                if (array_length > 2)
+                {
+                    if (dataType == "Decimal")
+                    {
+                        try
+                        {
+                            z_array_double[z_array_double.Length - 1] = Convert.ToDouble(input_array[2]);
+                            Array.Copy(z_array_double, 1, z_array_double, 0, z_array_double.Length - 1);
+                        }
+                        catch
+                        {
+                            Array.Copy(z_array_double, 1, z_array_double, 0, z_array_double.Length - 1);
+                        }
+                    }
+                    else
+                    {
+                        try
+                        {
+                            z_array_int[z_array_int.Length - 1] = Convert.ToInt16(input_array[1]);
+                            Array.Copy(z_array_int, 1, z_array_int, 0, z_array_int.Length - 1);
+                        }
+                        catch
+                        {
+                            Array.Copy(z_array_int, 1, z_array_int, 0, z_array_int.Length - 1);
+                        }
+                    }
+                }
+
+                if (chart1.IsHandleCreated && chart2.IsHandleCreated && chart3.IsHandleCreated)
+                {
+                    Invoke((MethodInvoker)delegate { UpdateCharts(); });
+                }
+
+                lock (m_Locker)
+                {
+                    run = signalThread;
+                }
+
+            }
+        }
+
+        private void UpdateCharts()
+        {
+            chart1.Series["X Axis"].Points.Clear();
+            chart2.Series["Y Axis"].Points.Clear();
+            chart3.Series["Z Axis"].Points.Clear();
+
+            if (dataType == "Decimal")
+            {
+                for (int i = 0; i < x_array_double.Length; i++)
+                {
+                    chart1.Series["X Axis"].Points.AddY(x_array_double[i]);
+                }
+
+                for (int i = 0; i < y_array_double.Length; i++)
+                {
+                    chart2.Series["Y Axis"].Points.AddY(y_array_double[i]);
+                }
+
+                for (int i = 0; i < z_array_double.Length; i++)
+                {
+                    chart3.Series["Z Axis"].Points.AddY(z_array_double[i]);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < x_array_int.Length; i++)
+                {
+                    chart1.Series["X Axis"].Points.AddY(x_array_int[i]);
+                }
+
+                for (int i = 0; i < y_array_int.Length; i++)
+                {
+                    chart2.Series["Y Axis"].Points.AddY(y_array_int[i]);
+                }
+
+                for (int i = 0; i < z_array_int.Length; i++)
+                {
+                    chart3.Series["Z Axis"].Points.AddY(z_array_int[i]);
+                }
+            }
+
+        }
+
+        private void comboBox1_DropDown(object sender, EventArgs e)
+        {
+            // rescan for comm ports
+            scanForCommPorts();
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+                    dataType = comboBox3.SelectedItem.ToString();
+        }
+
+    }
+}
